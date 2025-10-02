86954ee53c0685cf9cada2a2aceb43c091d62087
Alex Salgado <alexgsalgado@gmail.com>
2025-10-02 11:04:48 -0300

    fix: remove unused code

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import Router from './Router';
-import { firebaseApp, analytics } from "./utils/firebase";
 
 ReactDOM.render(
   <React.StrictMode>
